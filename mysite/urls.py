264cbba961cf90df2b137d8584cfaf166d52f424
J-Weeks <weeksjasons@gmail.com>
2015-05-09 22:45:38 -0400

    skeleton blog app able to run locally

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'blog.views.index'),
+    url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
 )
